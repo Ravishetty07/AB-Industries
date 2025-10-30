b64e1e168de20f90839335817a8f9a9cea81e030
Ravishetty07 <ravishetty05082@gmail.com>
2025-10-30 18:45:38 +0530

    Nav Bar UI Fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -63,7 +63,7 @@ const Header = () => {
         {/* Logo Section */}
         <Link href="/" className="flex items-center space-x-2">
           <Image
-            src="/logo.jpg" // make sure this file is inside /public folder
+            src="/logo2.png" // make sure this file is inside /public folder
             alt="AB Industries Logo"
             width={40}
             height={40}
